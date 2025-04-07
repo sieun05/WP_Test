089410dc4ff4072a79f4aeb0c02c862ad12a193a
sieun05 <dhtldms0125@naver.com>
2025-04-08 00:42:02 +0900

    0408 Home Ex2-11 end

--- WP_Test/Ex2-11.cpp
+++ WP_Test/Ex2-11.cpp
@@ -66,9 +66,12 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
     static int sel_obj{};
     static int r{}, g{}, b{};
 
-    static int key_down{};
+    static bool key_pressed[256] = { false };
+    static COLORREF defaultColor = RGB(255, 255, 255);
+    static COLORREF pressedColor{};
 
     static int rotate{};
+    static int shape_positions[4] = { 1, 2, 3, 4 };
 
     switch (iMsg) {
     case WM_CREATE:
@@ -88,22 +91,20 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
     //    break;
     case WM_CHAR:
     {
+        key_pressed[wParam] = true;
+
         switch (wParam) {
         case 'c':   //원 선택
             sel_obj = 1;
-            r = rand() % 255, g = rand() % 255, b = rand() % 255;
             break;
         case 's': //모래시계 선택
             sel_obj = 2;
-            r = rand() % 255, g = rand() % 255, b = rand() % 255;
             break;
         case 'p':   //오각형 선택
             sel_obj = 3;
-            r = rand() % 255, g = rand() % 255, b = rand() % 255;
             break;
         case 'e':   //파이모양 선택
             sel_obj = 4;
-            r = rand() % 255, g = rand() % 255, b = rand() % 255;
             break;
         case 'q':
         case 'Q':
@@ -113,14 +114,33 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
         break;
     }
     case WM_KEYDOWN:
+        if (wParam < 256) key_pressed[tolower(wParam)] = true;
+
         switch (wParam) {;
         case VK_LEFT:
-            rotate++;
-            break;
-        case VK_RIGHT:
-            rotate--;
+        {
+            int temp = shape_positions[0];
+            for (int i = 0; i < 3; ++i)
+                shape_positions[i] = shape_positions[i + 1];
+            shape_positions[3] = temp;
+            sel_obj = shape_positions[1];
             break;
         }
+        case VK_RIGHT:
+        {
+            int temp = shape_positions[3];
+            for (int i = 3; i > 0; --i)
+                shape_positions[i] = shape_positions[i - 1];
+            shape_positions[0] = temp;
+            sel_obj = shape_positions[1];
+            break;
+        }
+        }
+
+        InvalidateRect(hWnd, NULL, TRUE);
+        break;
+    case WM_KEYUP:
+        if (wParam < 256) key_pressed[tolower(wParam)] = false;
         InvalidateRect(hWnd, NULL, TRUE);
         break;
     case WM_PAINT:
@@ -132,8 +152,6 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
 
         RECT rect{ client_rect.right / 2. - 75 , client_rect.bottom / 2. - 75,  client_rect.right / 2. + 75,  client_rect.bottom / 2. + 75 }; //200 -25
 
-        //RECT rect{ 225, 175, 375, 325 }; //200 -25
-
         hPen = CreatePen(PS_SOLID, 3, RGB(0, 0, 0));
         old_pen = (HPEN)SelectObject(hdc, hPen);
 
@@ -142,10 +160,17 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
         RECT rect_2{ rect };
         InflateRect(&rect_2, -20, -20);
 
- 
-        hBrush = CreateSolidBrush(RGB(r, g, b));
-        old_brush = (HBRUSH)SelectObject(hdc, hBrush);
+        bool isHeld = key_pressed['c'] || key_pressed['s'] || key_pressed['p'] || key_pressed['e'];
+        if (isHeld) {
+            r = rand() % 255; g = rand() % 255; b = rand() % 255;
+            pressedColor = RGB(r, g, b);
+        }
+        else {
+            pressedColor = defaultColor;
+        }
 
+        HBRUSH hBrush = CreateSolidBrush(pressedColor);
+        HBRUSH oldBrush = (HBRUSH)SelectObject(hdc, hBrush);
 
         switch (sel_obj) {
         case 1:
@@ -175,67 +200,142 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
         SelectObject(hdc, old_brush);
         DeleteObject(hBrush);
 
+        int dx[4] = { 0, 160, 0, -160 };
+        int dy[4] = { -160, 0, 160, 0 };
 
-        OffsetRect(&rect, 0, 160);
-        //FillRect(hdc, &rect, CreateSolidBrush(RGB(220, 220, 220)));
-        //FrameRect(hdc, &rect, (HBRUSH)GetStockObject(BLACK_BRUSH)); //아래
+        for (int i = 0; i < 4; ++i) {
+            RECT r = rect;
+            OffsetRect(&r, dx[i], dy[i]);
+            RECT r2 = r;
+            InflateRect(&r2, -20, -20);
 
-        hBrush = CreateSolidBrush(RGB(0, 255, 0));
-        old_brush = (HBRUSH)SelectObject(hdc, hBrush);
+            // 도형 색상 지정
+            //COLORREF color;
+            //switch (i) {
+            //case 0: color = RGB(255, 0, 0); break; // 위쪽
+            //case 1: color = RGB(255, 255, 0); break; // 오른쪽
+            //case 2: color = RGB(0, 255, 0); break; // 아래쪽
+            //case 3: color = RGB(15, 15, 255); break; // 왼쪽
+            //}
 
-        rect_2 = rect;
-        InflateRect(&rect_2, -20, -20);
-        Pie(hdc, rect_2.left, rect_2.top, rect_2.right, rect_2.bottom, (rect_2.left + rect_2.right) / 2., rect_2.top, rect_2.right, (rect_2.top + rect_2.bottom) / 2.);
+            
 
-        SelectObject(hdc, old_brush);
-        DeleteObject(hBrush);
+            switch (shape_positions[i]) {
+            case 1: // 원
+                hBrush = CreateSolidBrush(RGB(0, 0, 255));
+                old_brush = (HBRUSH)SelectObject(hdc, hBrush);
+                
+                Ellipse(hdc, r2.left, r2.top, r2.right, r2.bottom);
 
-        OffsetRect(&rect, 0, -320);
-        //FillRect(hdc, &rect, CreateSolidBrush(RGB(220, 220, 220)));
-        //FrameRect(hdc, &rect, (HBRUSH)GetStockObject(BLACK_BRUSH)); //위
+                SelectObject(hdc, old_brush);
+                DeleteObject(hBrush);
+                break;
+            case 2: // 모래시계 (사각형 2개)
+            {
+                hBrush = CreateSolidBrush(RGB(255, 0, 0));
+                old_brush = (HBRUSH)SelectObject(hdc, hBrush);
 
-        hBrush = CreateSolidBrush(RGB(255, 0, 0));
-        old_brush = (HBRUSH)SelectObject(hdc, hBrush);
+                POINT point_tt[8]{ {r2.left, r2.top}, {r2.right, r2.top}, {r2.left, r2.bottom}, {r2.right, r2.bottom} };
+                Polygon(hdc, point_tt, 4);
 
-        rect_2 = rect;
-        InflateRect(&rect_2, -20, -20);
-        POINT point_tt[8]{ {rect_2.left, rect_2.top}, {rect_2.right, rect_2.top}, {rect_2.left, rect_2.bottom}, {rect_2.right, rect_2.bottom} };
-        Polygon(hdc, point_tt, 4);
+                SelectObject(hdc, old_brush);
+                DeleteObject(hBrush);
+                break;
+            }
+            case 3: // 오각형
+            {
+                hBrush = CreateSolidBrush(RGB(255, 255, 0));
+                old_brush = (HBRUSH)SelectObject(hdc, hBrush);
 
-        SelectObject(hdc, old_brush);
-        DeleteObject(hBrush);
+                POINT point_p[10]{ 
+                    {(r2.left + r2.right) / 2., r2.top}, 
+                    {r2.right ,(r2.top + r2.bottom) / 2. - 10}, 
+                    {r2.right - 30, r2.bottom}, 
+                    {r2.left + 30, r2.bottom}, 
+                    {r2.left ,(r2.top + r2.bottom) / 2. - 10} };
+                Polygon(hdc, point_p, 5);
 
-        OffsetRect(&rect, 160, 160);
-        //FillRect(hdc, &rect, CreateSolidBrush(RGB(220, 220, 220)));
-        //FrameRect(hdc, &rect, (HBRUSH)GetStockObject(BLACK_BRUSH)); //오른쪽
+                SelectObject(hdc, old_brush);
+                DeleteObject(hBrush);
+                break;
+            }
+            case 4: // 파이
+                hBrush = CreateSolidBrush(RGB(0, 255, 0));
+                old_brush = (HBRUSH)SelectObject(hdc, hBrush);
 
-        hBrush = CreateSolidBrush(RGB(255, 255, 0));
-        old_brush = (HBRUSH)SelectObject(hdc, hBrush);
+                Pie(hdc, r2.left, r2.top, r2.right, r2.bottom, 
+                    (r2.left + r2.right) / 2., r2.top, 
+                    r2.right, (r2.top + r2.bottom) / 2.);
 
-        rect_2 = rect;
-        InflateRect(&rect_2, -20, -20);
-        POINT point_p[10]{ {(rect_2.left + rect_2.right) / 2., rect_2.top}, {rect_2.right ,(rect_2.top + rect_2.bottom) / 2. - 10}, {rect_2.right - 30, rect_2.bottom}, {rect_2.left + 30, rect_2.bottom}, {rect_2.left ,(rect_2.top + rect_2.bottom) / 2. - 10} };
-        Polygon(hdc, point_p, 5);
+                SelectObject(hdc, old_brush);
+                DeleteObject(hBrush);
+                break;
+            }
+            SelectObject(hdc, old_brush);
+            DeleteObject(hBrush);
+        }
 
-        SelectObject(hdc, old_brush);
-        DeleteObject(hBrush);
 
-        OffsetRect(&rect, -320, 0);
-        //FillRect(hdc, &rect, CreateSolidBrush(RGB(220, 220, 220)));
-        //FrameRect(hdc, &rect, (HBRUSH)GetStockObject(BLACK_BRUSH)); //왼쪽
+        //OffsetRect(&rect, 0, 160);
+        ////FillRect(hdc, &rect, CreateSolidBrush(RGB(220, 220, 220)));
+        ////FrameRect(hdc, &rect, (HBRUSH)GetStockObject(BLACK_BRUSH)); //아래
 
-        hBrush = CreateSolidBrush(RGB(15, 15, 255));
-        old_brush = (HBRUSH)SelectObject(hdc, hBrush);
+        //hBrush = CreateSolidBrush(RGB(0, 255, 0));
+        //old_brush = (HBRUSH)SelectObject(hdc, hBrush);
 
-        rect_2 = rect;
-        InflateRect(&rect_2, -20, -20);
-        Ellipse(hdc, rect_2.left, rect_2.top, rect_2.right, rect_2.bottom);
+        //rect_2 = rect;
+        //InflateRect(&rect_2, -20, -20);
+        //Pie(hdc, rect_2.left, rect_2.top, rect_2.right, rect_2.bottom, (rect_2.left + rect_2.right) / 2., rect_2.top, rect_2.right, (rect_2.top + rect_2.bottom) / 2.);
 
-        SelectObject(hdc, old_brush);
-        SelectObject(hdc, old_pen);
+        //SelectObject(hdc, old_brush);
+        //DeleteObject(hBrush);
 
-        DeleteObject(hBrush);
-        DeleteObject(hPen);
+        //OffsetRect(&rect, 0, -320);
+        ////FillRect(hdc, &rect, CreateSolidBrush(RGB(220, 220, 220)));
+        ////FrameRect(hdc, &rect, (HBRUSH)GetStockObject(BLACK_BRUSH)); //위
+
+        //hBrush = CreateSolidBrush(RGB(255, 0, 0));
+        //old_brush = (HBRUSH)SelectObject(hdc, hBrush);
+
+        //rect_2 = rect;
+        //InflateRect(&rect_2, -20, -20);
+        //POINT point_tt[8]{ {rect_2.left, rect_2.top}, {rect_2.right, rect_2.top}, {rect_2.left, rect_2.bottom}, {rect_2.right, rect_2.bottom} };
+        //Polygon(hdc, point_tt, 4);
+
+        //SelectObject(hdc, old_brush);
+        //DeleteObject(hBrush);
+
+        //OffsetRect(&rect, 160, 160);
+        ////FillRect(hdc, &rect, CreateSolidBrush(RGB(220, 220, 220)));
+        ////FrameRect(hdc, &rect, (HBRUSH)GetStockObject(BLACK_BRUSH)); //오른쪽
+
+        //hBrush = CreateSolidBrush(RGB(255, 255, 0));
+        //old_brush = (HBRUSH)SelectObject(hdc, hBrush);
+
+        //rect_2 = rect;
+        //InflateRect(&rect_2, -20, -20);
+        //POINT point_p[10]{ {(rect_2.left + rect_2.right) / 2., rect_2.top}, {rect_2.right ,(rect_2.top + rect_2.bottom) / 2. - 10}, {rect_2.right - 30, rect_2.bottom}, {rect_2.left + 30, rect_2.bottom}, {rect_2.left ,(rect_2.top + rect_2.bottom) / 2. - 10} };
+        //Polygon(hdc, point_p, 5);
+
+        //SelectObject(hdc, old_brush);
+        //DeleteObject(hBrush);
+
+        //OffsetRect(&rect, -320, 0);
+        ////FillRect(hdc, &rect, CreateSolidBrush(RGB(220, 220, 220)));
+        ////FrameRect(hdc, &rect, (HBRUSH)GetStockObject(BLACK_BRUSH)); //왼쪽
+
+        //hBrush = CreateSolidBrush(RGB(15, 15, 255));
+        //old_brush = (HBRUSH)SelectObject(hdc, hBrush);
+
+        //rect_2 = rect;
+        //InflateRect(&rect_2, -20, -20);
+        //Ellipse(hdc, rect_2.left, rect_2.top, rect_2.right, rect_2.bottom);
+
+        //SelectObject(hdc, old_brush);
+        //SelectObject(hdc, old_pen);
+
+        /*DeleteObject(hBrush);
+        DeleteObject(hPen);*/
         EndPaint(hWnd, &ps);
         break;
     }
